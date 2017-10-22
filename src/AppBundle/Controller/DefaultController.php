a61ca3431c343496c175e5604c4b9bd51db3a1ec
Veaceslav Falceanu <veaceslav.falceanu@inresonance.com>
2017-10-21 22:39:26 -0400

    Rename the text

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,6 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return new Response('Providence Youth');
+        return new Response('Providence Youth Controller');
     }
 }
