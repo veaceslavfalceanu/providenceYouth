2671a641fca12ade90ca83ab9ad86400e9b2902b
Veaceslav Falceanu <veaceslav.falceanu@inresonance.com>
2017-10-21 22:33:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+providenceYouth
+===============
+
+A Symfony project created on October 22, 2017, 4:11 am.
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * File User.php
+ *
+ * @author Veaceslav Falceanu <veaceslav.falceanu@inresonance.com>
+ */
+
+namespace AppBundle\Entity;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $email;
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getPassword()
+    {
+        // TODO: Implement getPassword() method.
+    }
+
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+}
