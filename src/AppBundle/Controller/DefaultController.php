282304b8d6d8ffafdefefc73c7110fe678fba836
Veaceslav Falceanu <veaceslav.falceanu@inresonance.com>
2017-10-21 22:47:34 -0400

    Install php unit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,7 +5,6 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -14,6 +13,9 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return new Response('Providence Youth Controller');
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
     }
 }
